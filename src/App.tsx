46c56c7baa365e272affddcaa8eecc5c35502566
Hoang Hy <hoanghy01122003@gmail.com>
2023-09-14 18:44:44 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -115,6 +115,7 @@ function App() {
 														onClick={() =>
 															handleSendText(question)
 														}
+                            className=" text-left"
 													>
 														{question}
 													</button>
